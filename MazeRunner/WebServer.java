6b7d702134cbb43ebf0efc052064c657acacadd4
Haruka Nanase <andrecaetanosoares@gmail.com>
2018-04-10 01:29:26 +0100

    Added some fixes and other stuff. Re-write of the handler to handle the request and generate the maze response.

--- MazeRunner/WebServer.java
+++ MazeRunner/WebServer.java
@@ -12,6 +12,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.UUID;
+
 public class WebServer {
 
     private static String CURRENT_PATH;
@@ -19,40 +21,33 @@ public class WebServer {
         Path currentRelativePath = Paths.get("");
         CURRENT_PATH = currentRelativePath.toAbsolutePath().toString();
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/mzrun.html", new MyHandler());
+        server.createContext("/mzrun.html", new MazeHandler());
         server.setExecutor(java.util.concurrent.Executors.newCachedThreadPool()); // creates a default executor
         server.start();
     }
 
-    static class MyHandler implements HttpHandler {
+    static class MazeHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            //String response = "This was the query:" + t.getRequestURI().getQuery() + "##";
-            String response = SolveMaze(t.getRequestURI().getQuery());
-            //t.getRequestHeaders().add("Content-Type", "text/html");
-            t.sendResponseHeaders(200, response.length());
+            byte[] response = SolveMaze(t.getRequestURI().getQuery());
+            t.sendResponseHeaders(200, response.length);
             OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
+            os.write(response);
             os.close();
         }
     }
 
-    public static String SolveMaze(String args){
+    public static byte[] SolveMaze(String args){
         try{
             String[] final_args = GetQueryValues(args);
+            System.out.println("Solving maze...");
             Main.main(final_args);
             StringBuilder sb = new StringBuilder();
             String path = CURRENT_PATH + "/" + final_args[7];
-            try (BufferedReader r = Files.newBufferedReader(Paths.get(path), Charset.forName("UTF-8"))) {
-                String s;
-                while((s = r.readLine()) != null)
-                    sb.append(s);
-            }
-            return sb.toString();
-
+            return Files.readAllBytes(Paths.get(path));
 
         }catch(Exception e){
-            return "Error";
+            return "Something went terribly wrong.".getBytes();
         }
     }
 
@@ -61,12 +56,12 @@ public class WebServer {
         ArrayList<String> list = new ArrayList<String>(Arrays.asList(args_array));
         list.add(list.get(0));
         list.remove(0);
-        String fileName = "GenerateRandomName.html";
-        list.add(fileName);
+        String fileName = "solutions/"+UUID.randomUUID().toString();
         for(int i = 0; i < list.size(); i++){
             String s = list.get(i);
             list.set(i,s.substring(s.lastIndexOf("=")+1));
         }
+        list.add(fileName);
         return list.toArray(new String[list.size()]);
     }
 
